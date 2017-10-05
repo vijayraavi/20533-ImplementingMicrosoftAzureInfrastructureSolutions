457effa6b42f0c18171b04c2a15137f0e902a39e
MP <MP>
2017-10-05 08:44:13 -0700

    cleanup

--- Allfiles/Labfiles/Lab03/Starter/Templates/Deploy-AzureResourceGroup.ps1
+++
@@ -1,5 +0,0 @@
-$deploymentName	= "WebTierVM1-Deployment"
-$templateFile = 'E:\Labfiles\Lab03\Starter\Templates\azuredeploywebvm.json'
-$rgName	= "20533D0301-LabRG"
-
-New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $rgName -TemplateFile $templateFile
